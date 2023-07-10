d43152492dd59cd8cfcf855d584a5fe4a017e31a
DylanQin4 <dylanqin4@gmail.com>
2023-07-10 11:36:07 +0200

    Update bdd 2

--- script.sql
+++ script.sql
@@ -21,7 +21,7 @@ CREATE TABLE histo_morphology (
     id_users int REFERENCES users(id),
     taille int NOT NULL,
     poids int NOT NULL,
-    updated_at datetime DEFAULT NULL
+    updated_at datetime NOT NULL
 );
 
 CREATE TABLE code (
@@ -39,42 +39,71 @@ CREATE TABLE money_code (
 CREATE TABLE wallet (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     id_users int REFERENCES users(id),
-    money numeric(8,2) NOT NULL
+    money numeric(8,2) 
 );
 
 CREATE TABLE histo_wallet (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     id_users int REFERENCES users(id),
     money int NOT NULL,
-    updated_at datetime DEFAULT NULL
+    updated_at datetime NOT NULL
+);
+
+create table regime_type(
+    id int primary key auto_increment,
+    type varchar(255) 
+);
+
+create table aliments (
+    id int primary key auto_increment,
+    nom varchar(255) not null, 
+    id_type int references regime_type(id) not null,
+    prix numeric(8,2) not null
+);
+
+create table sports (
+    id int primary key auto_increment,
+    id_type int references regime_type(id) not null,
+    nom varchar(255) not null
+)
+
+create table regime_aliment(
+    id int primary key auto_increment
+);
+
+create table details_aliments(
+    id int primary key auto_increment,
+    id_regime_aliment int references regime_aliment(id) not null,
+    id_aliments int references aliments(id) not null,
+    poids numeric(6,2) not null
+);
+
+create table regime_sportif(
+    id int primary key auto_increment
+);
+
+create table details_sportif(
+    id int primary key auto_increment,
+    id_regime_sportif int references regime_sportif(id) not null,
+    id_sports int references sports(id) not null,
+    duree numeric(5,2)
 );
 
 create table regime(
     id int primary key auto_increment,
-    id_utilisateur int references utilisateur(id) not null,
+    id_user int references users(id) not null,
     id_aliment int references regime_aliment(id) not null,
-    id_sport references regime_sportif(id) not null,
-    taille int,
-    poids int
-);
-create table regime_type(
-    id int primary key auto_increment,
-    type varchar(50),
-);
-create table regime_aliment(
-    id int primary key auto_increment,
-    id_type int references regime_type(id) not null,
-    Nom varchar(250) not null,
-    prix int,not null
+    id_sport int references regime_sportif(id) not null,
+    poids int not null,
+    date_regime date not null
 );
 
-create table regime_sportif(
+create table histo_regime (
     id int primary key auto_increment,
-    id_type int references regime_type(id) not null,
-    nom varchar(100) not null,
-    poids int
+    id_user int references users(id) not null,
+    id_regime int references regime(id) not null,
+    date_debut date not null,
+    date_fin date not null
 );
 
-insert into regime_aliment values(default,1,'Vary/anana',5000);
-insert into regime_aliment values(default,1,'Vary/legume',5000);
-insert into regime_aliment values(default,1,'Vary/Salade',4000);
+
