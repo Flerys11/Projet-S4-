4c88499de5e66c05cf38fd9014d2cf49521ea15d
DylanQin4 <dylanqin4@gmail.com>
2023-07-11 06:20:17 +0300

    generated regime v2

--- application/models/PlatModel.php
+++ application/models/PlatModel.php
@@ -20,4 +20,28 @@ class PlatModel extends CI_Model {
         $this->db->where('id', $id);
         return $this->db->get();
     }
+
+    public function get_nb_type_plat(){
+        $this->db->from('type_plat');
+        return $this->db->count_all_results();
+    }
+
+    public function get_all_plat_ids_by_type($id_type_regime){
+        $this->db->select('id');
+        $this->db->from('plat');
+        $this->db->where('id_type_regime', $id_type_regime);
+        $query = $this->db->get();
+        
+        $platIds = array();
+        
+        if ($query->num_rows() > 0) {
+            foreach ($query->result_array() as $row) {
+                $platIds[] = $row['id'];
+            }
+        }
+        
+        return $platIds;
+    }
+
+    
 }
